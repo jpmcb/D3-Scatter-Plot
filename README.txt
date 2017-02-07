8b1a01e09eba9c3cd4b8aedd6969db9e33c02bdf
John McBride <jpmmcbride@gmail.com>
2017-02-07 06:38:57 -0700

    Update to Index and JS file
    1. Added dynamic tooltip that appears just below mouse
    2. Updated CSS & SCSS colors and appearances
    3. Update to spacing and comments throughout

--- README.txt
+++ README.txt
@@ -1,3 +1,4 @@
-A Pen created at CodePen.io. You can find this one at http://codepen.io/jpmcb/pen/VPxgXZ.
+# D3 Scatter Plot
+_____
 
- 
+Using the [D3 libray](https://d3js.org/), this scatter plot graph shows Tour De France times & allegations of dopping in professional bicycle racing. Uses data provided from FreeCodeCamp [here]('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json').
--- js/index.js
+++ js/index.js
@@ -20,37 +20,93 @@ function runData() {
     return '<h3>' + myData[i]['Name'] + '</h3><p><strong>Time:</strong> ' + myData[i]['Time'] + '<br><strong>Place:</strong> ' + myData[i]['Place'] + '<br>' + myData[i]['Doping'] + '</p>';
   });
 
-  svg.append('line') //x-axis line
-  .attr('id', 'xaxis').attr('x1', '10').attr('y1', '490').attr('x2', '890').attr('y2', '490');
-  svg.append('text').attr('id', 'xaxis-text').attr('x', '427').attr('y', '480').text('Speed');
-  svg.append('text').attr('class', 'axis-desc').attr('x', '840').attr('y', '475').text('Fastest');
+  svg.append('line') //x-axis line and text
+    .attr('id', 'xaxis')
+    .attr('x1', '10')
+    .attr('y1', '490')
+    .attr('x2', '890')
+    .attr('y2', '490');
 
-  svg.append('line') //y-axis line
-  .attr('id', 'yaxis').attr('x1', '10').attr('y1', '10').attr('x2', '10').attr('y2', '490');
-  svg.append('text').attr('id', 'yaxis-text').attr('x', '20').attr('y', '250').text('Ranking');
-  svg.append('text').attr('class', 'axis-desc').attr('x', '25').attr('y', '25').text('First');
-  svg.append('text').attr('class', 'axis-desc').attr('x', '20').attr('y', '480').text('0');
-  svg.append('circle').attr('cx', '60').attr('cy', '60').attr('r', '5').attr('fill', 'purple');
-  svg.append('text').attr('x', '70').attr('y', '64').text('Doping Allegations');
-  svg.append('circle').attr('cx', '60').attr('cy', '80').attr('r', '5');
-  svg.append('text').attr('x', '70').attr('y', '86').text('No Doping Allegations');
+  svg.append('text')
+    .attr('id', 'xaxis-text')
+    .attr('x', '427')
+    .attr('y', '480')
+    .text('Speed');
+
+  svg.append('text')
+    .attr('class', 'axis-desc')
+    .attr('x', '840')
+    .attr('y', '475')
+    .text('Fastest');
+
+  svg.append('line') //y-axis line and text
+    .attr('id', 'yaxis')
+    .attr('x1', '10')
+    .attr('y1', '10')
+    .attr('x2', '10')
+    .attr('y2', '490');
+
+  svg.append('text')
+    .attr('id', 'yaxis-text')
+    .attr('x', '20')
+    .attr('y', '250')
+    .text('Ranking');
+
+  svg.append('text')
+    .attr('class', 'axis-desc')
+    .attr('x', '25')
+    .attr('y', '25')
+    .text('First');
+
+  svg.append('text')
+    .attr('class', 'axis-desc')
+    .attr('x', '20')
+    .attr('y', '480')
+    .text('0');
+
+  svg.append('circle') //circles & text for legend of graph
+    .attr('cx', '60')
+    .attr('cy', '60')
+    .attr('r', '5')
+    .attr('fill', 'purple');
+
+  svg.append('text')
+    .attr('x', '70')
+    .attr('y', '64')
+    .text('Doping Allegations');
+
+  svg.append('circle')
+    .attr('cx', '60')
+    .attr('cy', '80')
+    .attr('r', '5');
+
+  svg.append('text')
+    .attr('x', '70')
+    .attr('y', '86')
+    .text('No Doping Allegations');
 
   svg.selectAll('circle.data') //data points across scatter plot graph
-  .data(speedData).enter().append('circle').attr('class', 'data').attr('cx', function (d) {
-    return (2400 - d) * 4.5;
-  }).attr('cy', function (d, i) {
-    return myData[i]['Place'] * 12 + 15;
-  }).attr('r', '4').attr('fill', function (d, i) {
-    if (myData[i]['Doping'] === '') {
-      return 'black';
-    } else {
-      return 'purple';
-    }
-  }).on('mouseover', function (d, i) {
-    return $('.hover' + i).css('display', 'inline-block');
-  }).on('mouseout', function (d, i) {
-    return $('.hover' + i).css('display', 'none');
-  });
+    .data(speedData)
+      .enter().append('circle')
+      .attr('class', 'data')
+      .attr('cx', function (d) {
+        return (2400 - d) * 4.5;
+      }).attr('cy', function (d, i) {
+        return myData[i]['Place'] * 12 + 15;
+      }).attr('r', '5')
+      .attr('fill', function (d, i) {
+        if (myData[i]['Doping'] === '') {
+          return 'black';
+        } else {
+          return 'purple';
+        }
+
+        //event handlers for tooltips to appear just below mouse and to the right
+      }).on('mouseover', function (d, i) {
+        return $('.hover' + i).css('display', 'inline-block').css('top', event.pageY + 15).css('left', event.pageX + 15);
+      }).on('mouseout', function (d, i) {
+        return $('.hover' + i).css('display', 'none');
+      });
 }
 
 $.getJSON(url, function (data) {
